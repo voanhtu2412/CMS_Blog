f8b63f3e0b2466e869d796f85394b999ae898b8a
Tuvda <vodinhanhtu24122002@gmail.com>
2026-03-31 16:25:22 +0700

    Base data

---
+++ src/TeduBlog.API/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeduBlog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
